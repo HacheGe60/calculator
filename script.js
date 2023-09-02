943ab81c1595c72c38b22312c2001eb24e462f5c
LoloDev <horaciodev1706@gmail.com>
2023-09-02 17:18:07 -0300

    Done

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let num1 = '';
-let operator = '';
 let num2 = '';
+let numA;
+let numB;
+let result;
+let operator = '';
 let operation = '';
 
 const viewerEl = document.querySelector('.viewer');
@@ -45,40 +48,94 @@ function operate(operator, num1, num2) {
 };
 
 keysEl.forEach(key => {
+    key.addEventListener('click', () => {
+        const keyText = key.textContent;
 
-    keysEl.addEventListener('click', () => {
-        if ()
-            if (operation === '') {
-                if (button.textContent === '') {
-                    button.addEventListener('click', () => {
-                        if (button.textContent === '.' && num1.length <= 9 && !num1.includes('.')) {
-                            num1 += '.';
-                            viewerEl.textContent = num1;
-                        } else if (num1.length <= 9) {
-                            if (button.textContent === '.') {
-                                num1 += '';
-                                return;
-                            } else {
-                                num1 += button.textContent;
-                                viewerEl.textContent = num1;
-                            }
-                        }
-                    });
-                } else {
-                    button.addEventListener('click', () => {
-                        if (button.textContent === '.' && num2.length <= 9 && !num2.includes('.')) {
-                            num2 += '.';
-                            viewerEl.textContent = num2;
-                        } else if (num2.length <= 9) {
-                            if (button.textContent === '.') {
-                                num2 += '';
-                                return;
-                            } else {
-                                num2 += button.textContent;
-                                viewerEl.textContent = num2;
-                            }
-                        }
-                    });
-                }
-            };
+        if (+keyText >= 0 && +keyText <= 9) {
+            getNumberClick(keyText);
+        } else if (keyText === '.') {
+            getDecimalClick();
+        } else if (keyText === '+ / -') {
+            getPlusMinusClick();
+        } else if (keyText === 'CE') {
+            getCEClick();
+        } else if (keyText === '>>') {
+            getBackspaceClick();
+        } else if (['+', '-', 'x', '/'].includes(keyText)) {
+            getOperatorClick(keyText);
+        } else if (keyText === '=') {
+            getEqualClick();
+        }
     });
+});
+
+function getNumberClick(keyText) {
+    if (operation === '') {
+        num1 += keyText;
+        viewerEl.textContent = num1;
+    } else {
+        num2 += keyText;
+        viewerEl.textContent = num2;
+    }
+}
+
+function getDecimalClick() {
+    if (!num1.includes('.') && !num2.includes('.')) {
+        if (operation === '') {
+            num1 += '.';
+            viewerEl.textContent = num1;
+        } else {
+            num2 += '.';
+            viewerEl.textContent = num2;
+        }
+    }
+}
+
+function getPlusMinusClick() {
+    if (operation === '') {
+        num1 = +num1 * -1;
+        viewerEl.textContent = num1;
+    } else {
+        num2 = +num2 * -1;
+        viewerEl.textContent = num2;
+    }
+}
+
+function getCEClick() {
+    num1 = '';
+    num2 = '';
+    operation = '';
+    viewerEl.textContent = '';
+}
+
+function getBackspaceClick() {
+    if (operation === '') {
+        num1 = num1.slice(0, -1);
+        viewerEl.textContent = num1;
+    } else {
+        num2 = num2.slice(0, -1);
+        viewerEl.textContent = num2;
+    }
+}
+
+function getOperatorClick(operator) {
+    if (operation !== '') {
+        return
+    }
+    numA = +num1;
+    operation = numA + operator;
+    num1 = '';
+    viewerEl.textContent = '';
+    return operator;
+}
+
+function getEqualClick() {
+    numB = +num2;
+    operator = operation.slice(-1);
+    result = operate(operator, numA, numB);
+    viewerEl.textContent = result;
+    num1 = '';
+    num2 = '';
+    operation = '';
+    operator = '';
+}
